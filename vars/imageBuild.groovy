6304d74d48d297850f7de12a9f12e51a5673ca04
Bright-O-Omubo <tokyoogram@gmail.com>
2026-09-20 05:20:00 +0100

    shared library source update

--- vars/imageBuild.groovy
+++ vars/imageBuild.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 
-def call(String imageName) {
-    echo "building the docker artifact for branch $BRANCH_NAME"
-    sh "docker build -t $imageName ."
+import com.example.Whale
 
+def call(String imageName) {
+    return new Whale(this).imageBuild(imageName)
 }
